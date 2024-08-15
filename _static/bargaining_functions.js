3c95aedaed9ed8aff5d7faf717272a743deecf24
NicolasRoever <nicoroever@gmail.com>
2024-08-15 14:34:53 +0200

    Write function to initializeSliders and call it in main script.

--- _static/bargaining_functions.js
+++ _static/bargaining_functions.js
@@ -18,6 +18,29 @@ function initializeElements() {
     window.SliderClicked = false
 }
 
+// Function to initialize the sliders
+function initializeSliders() {
+    // Initialize the first slider (my_slider)
+    window.my_slider = new mgslider("offer", 0, 10, 0.1);
+    my_slider.f2s = function (val) {
+        return '$' + val.toFixed(2);
+    };
+    my_slider.print(document.getElementById("my_slider"));
+    // Optional: Recall the value across errors
+    // my_slider.recall();
+
+    // Initialize the second slider (slider_other)
+    window.slider_other = new mgslider("other_offer", 0, 10, 0.1);
+    slider_other.f2s = function (val) {
+        return '$' + val.toFixed(2);
+    };
+    slider_other.print(document.getElementById("slider_other"));
+    slider_other.set(0);
+    slider_other.disable();
+    // Optional: Recall the value across errors
+    // slider_other.recall();
+}
+
 function createChart(chartName, yValues, yLabel, yMin, yMax) {
 new Chart(chartName, {
 type: "line",
