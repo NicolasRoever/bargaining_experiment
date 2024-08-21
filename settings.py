262fabec62f2bd1b9be5917791318bb94263e8e0
NicolasRoever <nicoroever@gmail.com>
2024-08-21 14:25:47 +0200

    Delete testing config from settings.

--- settings.py
+++ settings.py
@@ -9,15 +9,6 @@ SESSION_CONFIGS = [
     delay_treatment_high = True,
     information_asymmetry = "Buyer", # Which valuation is known? # Buyer, Seller or None
     treatment_communication = False,
-    ),
-    dict(
-    name = "bargain_live_testing",
-    app_sequence = ["bargain_live"],
-    num_demo_participants=6,
-    TA_treatment_high = False,
-    delay_treatment_high = True,
-    information_asymmetry = "Buyer", # Which valuation is known? # Buyer, Seller or None
-    treatment_communication = False,
     )
     
 ]
