14e6454b517f06bdfb0bf528dbff7c9899ece82f
NicolasRoever <nicoroever@gmail.com>
2025-02-07 10:31:50 +0100

    Add lexica.

---
+++ bargain_live/lexicons/lexicon_en.py
@@ -0,0 +1,14 @@
+class Lexicon:
+    match_value = "In this match, your value of the object is "
+    buyer_value_random = "The Buyer's value of the object will be randomly drawn between 0 and 60€."
+    bargaining_starts_in = "Bargaining starts in 5"
+    negotiation_costs = "Negotiation Costs"
+    current_negotiation_costs = "Current negotiation costs per 1 second: "
+    total_negotiation_costs = "Total negotiation costs: "
+    time = "Time: "
+    current_price_offer = "Your current submitted price offer: "
+    no_offer_yet = "No offer yet."
+    your_payoff = "Your payoff: "
+    seconds = "Seconds"
+    submit = "Submit"
+
