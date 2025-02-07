c5d51d5e4c5a2179282dafb99f0020aa52f18c25
jbauss2 <jbauss2@uni-koeln.de>
2025-02-07 10:31:11 +0100

    More translations added

---
+++ lexicons/lexicon_de.py
@@ -0,0 +1,7 @@
+class Lexicon:
+    instructions = "Erklärung"
+    welcome_message = "Willkommen zum Experiment. Bitte lesen Sie diese Anweisungen sorgfältig durch. Die Auszahlung, die Sie aus diesem Experiment erhalten, hängt teilweise von Ihren Entscheidungen, teilweise von den Entscheidungen anderer und teilweise vom Zufall ab. Den Betrag, den Sie verdienen, erhalten Sie via "
+    experiment_structure = "Das Experiment besteht aus 30 Runden. Zu Beginn des Experiments wird den Teilnehmenden zufällig die Rolle des Käufers oder des Verkäufers zugewiesen. Diese Rolle bleibt über alle 30 Runden hinweg gleich. Zu Beginn jeder Runde werden Sie zufällig einer anderen Person in der entgegengesetzten Rolle zugeordnet. Nach jeder Runde werden die Teilnehmenden neu zugeordnet, um neue Paare zu bilden."
+    negotiation_intro = "In jeder Runde verhandeln Käufer und Verkäufer über den Handel eines Objekts:"
+    seller_value = "Der Wert des Objekts für den Verkäufer beträgt 0 Euro."
+    buyer_value = "Der Wert des Objekts für den Käufer ist geheim und wird zufällig zwischen 0 und 60 Euro festgelegt. Nur der Käufer kennt seinen Wert, und dieser bleibt dem Verkäufer unbekannt."
